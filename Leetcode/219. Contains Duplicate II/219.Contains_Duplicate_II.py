9a6c94260655d85c108dc2c1b041e99ff8bb00eb
Shubhangi Dabral <44902363+ShubhangiDabral13@users.noreply.github.com>
2020-03-16 00:32:14 +0530

    Contain solution to the Leetcode 219 Question.

---
+++ Leetcode/219. Contains Duplicate II/219.Contains_Duplicate_II.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        a ={}
+        for i,n in enumerate(nums):
+            if n in a and i-a[n] <= k:
+                return True
+            else:
+                a[n] = i
+        return False
+            
