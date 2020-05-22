8771aa696d30b789d0d445dbb1efe298c3f026b1
Shubhangi Dabral <44902363+ShubhangiDabral13@users.noreply.github.com>
2020-05-22 20:41:24 +0530

    Add files via upload

---
+++ HackerRank/08).Queues: A Tale of Two Stacks/08).Queues A Tale of Two Stacks.py
@@ -0,0 +1,32 @@
+class MyQueue(object):
+    def __init__(self):
+        self.inward = []
+        self.outward = []
+
+    def peek(self):
+        if self.outward == []:
+            self.outward = list(reversed(self.inward))
+            self.inward = []
+        return self.outward[-1]
+
+    def pop(self):
+        head = self.peek()
+        del self.outward[-1]
+
+
+    def put(self, value):
+        self.inward.append(value)
+
+
+
+queue = MyQueue()
+t = int(input())
+for line in range(t):
+    values = map(int, input().split())
+    values = list(values)
+    if values[0] == 1:
+        queue.put(values[1])
+    elif values[0] == 2:
+        queue.pop()
+    else:
+        print(queue.peek())
